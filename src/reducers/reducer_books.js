b7a9614c636b1b5ff695f15453f21792e661ac33
ribhavh <ribhav.hora@wisc.edu>
2018-09-16 19:31:51 -0500

    Main Files

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,18 @@
+export default function() {
+	return [
+	{title: 'JS', pages: 'Pages - 1018989'},
+	{title: 'Harry Potter'},
+	{title: 'Archies Comics'},
+	{title: 'Dan Brown', pages: 101},
+	{title: 'Hulk 2'},
+	{title: 'The shooting star'},
+	{title: 'Suicide Squad', pages: 101},
+	{title: 'Comic Town'},
+	{title: 'Thor Comics'},
+	{title: 'Black Panther', pages: 101},
+	{title: 'Infinity War'},
+	{title: 'Avengers'}
+
+
+	];
+}
